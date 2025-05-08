9253a674d8da7e926986a04fdc5e79c0f86f8b05
uo271288 <uo271288@uniovi.es>
2025-05-08 10:25:38 +0200

    9 - Formularios basicos. 10 - Grids y cards

--- src/Components/Products/ListProductsComponent.js
+++ src/Components/Products/ListProductsComponent.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import {Card, Col, Image, Row} from 'antd';
 
 let ListProductsComponent = () => {
     let [products, setProducts] = useState([])
@@ -9,13 +10,11 @@ let ListProductsComponent = () => {
     }, [])
 
     let getProducts = async () => {
-        let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/products`,
-            {
-                method: "GET",
-                headers: {
-                    "apikey": localStorage.getItem("apiKey")
-                },
-            });
+        let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/products`, {
+            method: "GET", headers: {
+                "apikey": localStorage.getItem("apiKey")
+            },
+        });
 
         if (response.ok) {
             let jsonData = await response.json();
@@ -30,15 +29,18 @@ let ListProductsComponent = () => {
     }
 
 
-    return (
-        <div>
-            <h2>Products</h2>
-            <ul>
-                {products.map(p => <li key={p.id}> {p.title} {p.price} <Link to={"/products/edit/" + p.id}> Edit </Link>
-                </li>)}
-            </ul>
-        </div>
-    )
+    return (<div>
+        <h2>Products</h2>
+        <Row gutter={[16, 16]}>
+            {products.map(p => <Col span={8}>
+                <Link to={"/products/edit/" + p.id}>
+                    <Card hoverable key={p.id} title={p.title} cover={<Image src="/item1.png" preview={false}/>}>
+                        {p.price}
+                    </Card>
+                </Link>
+            </Col>)}
+        </Row>
+    </div>)
 }
 
 export default ListProductsComponent;
--- src/Components/User/LoginFormComponent.js
+++ src/Components/User/LoginFormComponent.js
@@ -1,20 +1,20 @@
 import {useState} from "react";
 import {modifyStateProperty} from "../../Utils/UtilsState";
 import {useNavigate} from "react-router-dom";
+import {Alert, Button, Card, Col, Form, Image, Input, Row} from "antd";
 
 let LoginFormComponent = () => {
+
     let navigate = useNavigate();
 
     let [formData, setFormData] = useState({
-        email: '',
-        password: '',
+        email: '', password: '',
     })
+    let [error, setError] = useState(false);
 
     let clickLogin = async () => {
         let response = await fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/users/login`, {
-            method: "POST",
-            headers: {"Content-Type": "application/json "},
-            body: JSON.stringify(formData)
+            method: "POST", headers: {"Content-Type": "application/json "}, body: JSON.stringify(formData)
         })
 
         if (response.ok) {
@@ -29,24 +29,53 @@ let LoginFormComponent = () => {
             let responseBody = await response.json();
             let serverErrors = responseBody.errors;
             serverErrors.forEach(e => {
-                console.log("Error: " + e.msg)
+                console.log("Error: " + e.msg);
             })
+            setError(true);
         }
     }
 
-    return (
-        <div>
-            <h2>Login User</h2>
-            <input onChange={(i) => {
-                modifyStateProperty(formData, setFormData, "email", i.currentTarget.value)
-            }} type="text" name="email"/>
-            <input onChange={(i) => {
-                modifyStateProperty(formData, setFormData, "password", i.currentTarget.value)
-            }} type="password" name="password"/>
-
-            <button onClick={clickLogin}>Login User</button>
-        </div>
-    )
+    return (<Row align="middle" justify="center" style={{minHeight: "70vh"}}>
+        <Col xs={0} sm={0} md={12} lg={8} xl={6}>
+            <Image src="/iniciar-sesion.png" width="100%"/>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12} xl={10}>
+            <Card title="Login" style={{width: "100%", margin: "15px"}}>
+                {error && <Alert style={{marginBottom: "2vh"}} type="error"
+                                 message="Algo ha ido mal, inténtelo más tarde"/>}
+                <Form
+                    name="login"
+                    initialValues={{remember: true}}
+                    onFinish={clickLogin}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label=""
+                        name="email"
+                        rules={[{required: true, message: "Por favor, introduce el email"}, {
+                            type: "email", message: "Por favor, introduce un email válido"
+                        },]}
+                    >
+                        <Input placeholder="Tu email" onChange={(i) => {
+                            modifyStateProperty(formData, setFormData, "email", i.currentTarget.value)
+                        }}/>
+                    </Form.Item>
+                    <Form.Item
+                        label=""
+                        name="password"
+                        rules={[{required: true, message: "Por favor, introduce la contraseña"}]}
+                    >
+                        <Input.Password placeholder="Tu contraseña" onChange={(i) => {
+                            modifyStateProperty(formData, setFormData, "password", i.currentTarget.value)
+                        }}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" block>Login User</Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </Col>
+    </Row>)
 }
 
 export default LoginFormComponent;
